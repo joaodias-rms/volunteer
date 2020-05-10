859d0975a27ad6d39414c992d65b6c6e4cfc427d
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-05-10 04:28:27 -0300

    Criada página da conta

---
+++ src/components/Account.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="">
+    <v-card flat>
+      <v-tabs
+        grow
+        v-model="tabActive"
+        dense
+        color="#8634eb"
+        slider-color="#8634eb"
+      >
+        <v-tab>
+          Ações realizadas
+        </v-tab>
+        <v-tab>
+          Minhas experiências
+        </v-tab>
+        <v-tab>
+          Configurar conta
+        </v-tab>
+
+          <v-tab-item>
+            <MyWorks/>
+          </v-tab-item>
+          <v-tab-item>
+            <Experiences />
+          </v-tab-item>
+          <v-tab-item>
+            <ProfileSettings />
+          </v-tab-item>
+      </v-tabs>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import MyWorks from "@/components/MyWorks.vue";
+import Experiences from "@/components/Experiences.vue";
+import ProfileSettings from "@/components/ProfileSettings.vue";
+export default {
+  components:{
+    MyWorks,
+    Experiences,
+    ProfileSettings
+  },
+    data:()=>({
+      tabActive: 0,
+    })
+};
+</script>
+
+<style></style>
