68c3753aa12ca9ad0677201f86dab2caafc401f9
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-05-10 04:30:33 -0300

    Atualizada rotas do perfil

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,10 @@ const routes = [
   {
     path: "/VolunteerWork",
     name: "VolunteerWork",
-    redirect: {name:"HeaderHome"},
+    redirect: { name: "HeaderHome" },
     // alias: {name:"VolunteerWork"},
     component: () => import("../views/VolunteerWork.vue"),
     children: [
-      
       {
         path: ":id/",
         name: "WorkDetail",
@@ -27,27 +26,42 @@ const routes = [
         path: "",
         name: "HeaderHome",
         component: () => import("../components/HeaderHome.vue"),
-      }
-
+      },
     ],
   },
   {
     path: "/Profile",
     name: "Profile",
     component: () => import("../views/Profile.vue"),
+    children: [
+      {
+        path: ":id/",
+        name: "MyWorksItem",
+        component: () => "../components/WorkDetail.vue",
+      },
+      {
+        path: "/",
+        name: "HeaderProfile",
+        component: () => import("../components/HeaderHome.vue"),
+      },
+    ],
   },
   {
     path: "/Save",
     name: "Save",
     component: () => import("../views/Save.vue"),
-    children:[
+    children: [
       {
         path: ":id/",
         name: "WorkDetailSaved",
         component: () => import("../components/WorkDetail.vue"),
       },
-
-    ]
+      {
+        path: "",
+        name: "HeaderSave",
+        component: () => import("../components/HeaderHome.vue"),
+      },
+    ],
   },
   {
     path: "/About",
