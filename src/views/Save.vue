93decc49dea5326898af9cc2830615d265324d17
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-05-10 00:48:34 -0300

    Criada view para os itens salvos

--- src/views/Save.vue
+++ src/views/Save.vue
@@ -1,13 +1,60 @@
 <template>
-  <div></div>
+  <div class="col-sm-12">
+    <v-row>
+      <div class="col-sm-2">
+        <Menu :menu="menu" />
+      </div>
+      <div class="col-sm-6">
+        <SavedItens />
+      </div>
+      <div class="col-sm-4">
+        <router-view> </router-view>
+      </div>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import SavedItens from "@/components/SavedItens.vue";
+import Menu from "@/components/Menu.vue";
+// import WorkDetail from "@/components/WorkDetail.vue";
 export default {
-
-}
+  name: "Save",
+  components: {
+    SavedItens,
+    Menu,
+    // WorkDetail,
+  },
+  data: () => ({
+    menu: [
+      {
+        id: 1,
+        name: "Home",
+        route: "/",
+        icon: "mdi-home"
+      },
+      {
+        id: 2,
+        name: "Profile",
+        route: "/Profile",
+        icon: "mdi-account"
+      },
+      {
+        id: 3,
+        name: "Saved Itens",
+        route: "/Save",
+        icon: "mdi-content-save"
+      },
+      {
+        id: 4,
+        name: "How it Works?",
+        route: "/About",
+        icon: "mdi-information-variant"
+      },
+    ],
+    Detail: true,
+  }),
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
