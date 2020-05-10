90c4a1921c86ca9e6b6f772fcfa5fad6a8f16e22
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-05-10 00:46:22 -0300

    Adicionado ícone e descrição dos itens do menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,21 +2,25 @@
   <div>
     <v-list>
       <v-list-item link :to="item.route" v-for="item in menu" :key="item.id">
-        <b> {{item.name}}</b>
+        <v-list-item-avatar>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>
+            <b> {{ item.name }}</b>
+          </v-list-item-title>
+        </v-list-item-content>
       </v-list-item>
     </v-list>
   </div>
 </template>
 
 <script>
-
 export default {
-    props:{
-        menu: Array
-    }
+  props: {
+    menu: Array,
+  },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
