7bea78c9c7fa6b39db24fd95d7923229ef2a3025
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-05-10 04:29:24 -0300

    Criado parte superior do perfil

---
+++ src/components/TopProfile.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="">
+    <v-card flat class="d-flex">
+      <v-img
+        height="240px"
+        width="250px"
+        src="https://avatars0.githubusercontent.com/u/61212858?s=460&u=eb895cb1e2b8f7c63aeef856c8ef6f34ec84a960&v=4"
+      >
+      </v-img>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            <p>João Victor Dias Ramos</p>
+          </v-list-item-title>
+          <v-list-item-content>
+            <p>Apaixonado pela vida e pelos animais</p>
+
+            <p>20 anos</p>
+
+            <p>Sinop - MT</p>
+            <div class="mt-3">
+              <v-icon class="align-content-start">mdi-star</v-icon>
+              <v-icon class="align-content-start">mdi-star</v-icon>
+              <v-icon class="align-content-start">mdi-star</v-icon>
+              <v-icon class="align-content-start">mdi-star</v-icon>
+              <v-icon class="align-content-start">mdi-star-outline</v-icon>
+            </div>
+          </v-list-item-content>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
