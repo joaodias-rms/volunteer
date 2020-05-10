a6fe9104bfdf62ff8ebf429b55a4e61f9c4a32f3
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-05-10 04:28:54 -0300

    Criada base para tabs

---
+++ src/components/Experiences.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <h3>tester</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ProfileSettings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <h3>testinho</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
