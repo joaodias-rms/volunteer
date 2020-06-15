0b85b55d1db703071cf35b187fc7fefa3c4966d1
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-06-14 22:09:28 -0300

    Criado componente toolbar

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,19 @@
+.<template>
+  <v-app-bar dense right flat>
+      <Icon Width="40%" Height="25px" colorIcon="#8634eb"/>
+    <v-spacer></v-spacer>
+    <v-btn outlined small color="#8634eb" to="/Login"> Login</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import Icon from "@/components/Icon.vue";
+export default {
+    components: {
+    Icon,
+  },
+};
+
+</script>
+
+<style></style>
