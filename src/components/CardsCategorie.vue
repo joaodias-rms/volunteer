005079a064bb777749ad4216f0335e1bfabeec5a
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-06-14 22:08:02 -0300

    Adicionada transparência nos cards

--- src/components/CardsCategorie.vue
+++ src/components/CardsCategorie.vue
@@ -1,5 +1,4 @@
 <template>
-  <v-app id="cardcategorie">
     <div class="col-sm-12">
       <div class="text-center mt-5">
         <h3 class="text-title">
@@ -9,8 +8,8 @@
       <v-layout class="col-sm-10 mt-5 mx-auto">
        <v-row class="mx-auto ml-3">
          <v-col sm="2" v-for="item in Categories" :key="item.id">
-          <v-card to="/VolunteerWork" flat>
-          <v-card flat>
+          <v-card to="/VolunteerWork" color="transparent" flat>
+          <v-card  flat>
             <v-img
               height="130px"
               width="130px"
@@ -18,7 +17,7 @@
             />
           </v-card>
           <div class="mt-2">
-            <span class="card-subtitle">
+            <span class="card-subtitle pa-2">
               <b>{{item.name}}</b>
             </span>
           </div>
@@ -27,7 +26,6 @@
        </v-row>
       </v-layout>
     </div>
-  </v-app>
 </template>
 
 <script>
