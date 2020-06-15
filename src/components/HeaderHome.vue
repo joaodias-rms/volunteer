e14434d091e7aee3cf6922cd5893b1fc7d16dbda
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-06-14 22:08:32 -0300

    Adicionado imagem à HeaderHome

--- src/components/HeaderHome.vue
+++ src/components/HeaderHome.vue
@@ -1,17 +1,27 @@
 <template>
-  <div id="HeaderIntro" class="mx-auto">
-    <div class="col-sm-10 mx-auto">
-      <div class="text-center">
-        <Icon />
+  <div class="mx-auto">
+    <v-img
+      dark
+      width="100%"
+      gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+      max-height="300px"
+      src="../assets/HomeImg.jpg"
+    >
+      <div class="fill-height bottom-gradient">
+        <div class="mx-auto mt-5">
+          <div class="text-center">
+            <Icon Width="100%" Height="30px" colorIcon="#FFF" />
+          </div>
+          <h3 class="text-center mx-auto col-sm-6">
+            Explore mais de 1.600+ projetos no exterior e encontre seu melhor
+            programa de voluntariado
+          </h3>
+        </div>
+        <div class="col-sm-4 mx-auto">
+          <v-btn x-large block dark color="#8634eb">Como funciona?</v-btn>
+        </div>
       </div>
-      <h3 class="text-center">
-        Explore mais de 1.600+ projetos no exterior e encontre seu melhor
-        programa de voluntariado
-      </h3>
-    </div>
-    <div class="col-sm-6 mx-auto">
-      <v-btn x-large block dark color="#8634eb">Como funciona?</v-btn>
-    </div>
+    </v-img>
   </div>
 </template>
 
@@ -25,8 +35,4 @@ export default {
 };
 </script>
 
-<style>
-#HeaderIntro {
-  margin-bottom: 50px;
-}
-</style>
+<style></style>
