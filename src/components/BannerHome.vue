201a33744aeff46569cbeb2fac9c8fb4de42095a
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-06-28 00:23:21 -0300

    Alterações de layout no banner

--- src/components/BannerHome.vue
+++ src/components/BannerHome.vue
@@ -2,58 +2,66 @@
   <v-layout class="col-sm-10 mt-5 mx-auto">
     <v-row justify="space-between">
       <v-col cols="auto" sm="4">
-        <v-card class=" text-center" color="transparent" flat>
+        <v-card class="mx-auto" max-width="230" color="transparent" flat>
           <v-img
-            class="mx-auto"
-            height="160px"
-            width="160px"
+            height="150"
             src="https://voluntarios.com.br/static/images/home/busca-peq.png"
           >
           </v-img>
-          <span>
-            <p class="mt-2">Busca de entidades</p>
-            <h6 class="text-center">
-              Procure por entidades que podem precisar de voluntários. Utilize
-              vários critérios na busca, como cidade, bairro e área de atuação
-            </h6>
-          </span>
+          <v-card-title>
+            <b> Busque entidades</b>
+          </v-card-title>
+          <v-card-text centered>
+            Procure por entidades que podem precisar de voluntários. Utilize
+            vários critérios na busca, como cidade, bairro e área de atuação
+          </v-card-text>
+          <v-card-actions>
+            <v-btn class="mx-auto" color="#8634eb" outlined dense large
+              >Saiba mais</v-btn
+            >
+          </v-card-actions>
         </v-card>
       </v-col>
       <v-col cols="auto" sm="4">
-        <v-card flat color="transparent" class=" text-center">
+        <v-card class="mx-auto" max-width="230" color="transparent" flat>
           <v-img
-            class="mx-auto"
-            height="160px"
-            width="160px"
-            src="https://voluntarios.com.br/static/images/home/gmaps-brasil-peq.png"
+            height="150"
+            src="https://pbs.twimg.com/media/EBj5cOGXYAAnTYC?format=jpg&name=small"
           >
           </v-img>
-          <span>
-            <p class="mt-2">Mapa de entidades</p>
-            <h6 class="text-center">
-              Veja onde se localizam as entidades cadastradas no sistema. Digite
-              um endereço para encontrar aquelas que estão mais próximas de
-              você.
-            </h6>
-          </span>
+          <v-card-title>
+            <b> Mapa de entidades</b>
+          </v-card-title>
+          <v-card-text centered>
+            Veja onde se localizam as entidades cadastradas no sistema. Digite
+            um endereço para encontrar aquelas que estão mais próximas de você.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn class="mx-auto" color="#8634eb" outlined dense large
+              >Saiba mais</v-btn
+            >
+          </v-card-actions>
         </v-card>
       </v-col>
       <v-col cols="auto" sm="4">
-        <v-card color="transparent" class=" text-center" flat>
+        <v-card class="mx-auto" max-width="230" color="transparent" flat>
           <v-img
-            class="mx-auto"
-            height="160px"
-            width="160px"
+            height="150"
             src="https://voluntarios.com.br/static/images/home/cadastro-peq.png"
           >
           </v-img>
-          <span>
-            <p class="mt-2">Cadastro de entidades</p>
-            <h6 class="text-center">
-              Gestor, cadastre sua entidade aqui, aumentando a visibilidade da
-              instituição para pessoas interessadas em ajudar.
-            </h6>
-          </span>
+          <v-card-title>
+            <b>Cadastro de ONGs</b>
+          </v-card-title>
+          <v-card-text centered>
+            Gestor, cadastre sua entidade aqui, aumentando a visibilidade da
+            instituição para pessoas interessadas em ajudar.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn class="mx-auto" color="#8634eb" outlined dense large
+              >Saiba mais</v-btn
+            >
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
