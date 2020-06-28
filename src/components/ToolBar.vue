6590142a0c99b0a8411d7ccd3ff4ab77085f52e7
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-06-28 00:25:43 -0300

    Adicionado link pra home no ícone

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,6 +1,8 @@
 .<template>
-  <v-app-bar dense right flat>
-      <Icon Width="40%" Height="25px" colorIcon="#8634eb"/>
+  <v-app-bar dense right flat max-height="45px">
+    <v-btn to="/" color="transparent" width="120" text class="justify-center" depressed small>
+      <Icon Width="120" Height="25px" colorIcon="#8634eb"
+    /></v-btn>
     <v-spacer></v-spacer>
     <v-btn outlined small color="#8634eb" to="/Login"> Login</v-btn>
   </v-app-bar>
@@ -9,11 +11,10 @@
 <script>
 import Icon from "@/components/Icon.vue";
 export default {
-    components: {
+  components: {
     Icon,
   },
 };
-
 </script>
 
 <style></style>
