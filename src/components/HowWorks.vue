b486679810f9e545dd96681b1b2f3fae0e64a562
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-06-28 00:25:14 -0300

    Criada página de como funciona o projeto

---
+++ src/components/HowWorks.vue
@@ -0,0 +1,41 @@
+.<template>
+  <div class="">
+    <v-img
+      dark
+      width="100%"
+      gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+      max-height="230px"
+      src="../assets/Volunteers.jpeg"
+    >
+    
+      <div class="fill-height bottom-gradient">
+        <h1 class=" mt-5 text-center mx-auto col-sm-6">Como atuamos</h1>
+      </div>
+    </v-img>
+    <div class="col-sm-10 mx-auto">
+      <h1 text-color="#8634eb">Programas</h1>
+      <p>
+        Para transformar vidas, formando profissionais de excelência e cidadãos
+        íntegros, o Instituto Ponte aplica a metodologia do programa Bom Aluno,
+        que possui histórico e solidez nos resultados também em outras
+        localidades brasileiras. O Instituto Bom Aluno do Brasil foi fundado em
+        1993 no Paraná, tendo sua metodologia replicada em Curitiba, Londrina,
+        Belo Horizonte e Salvador. Em 2019, foi instituído o Programa Migrar,
+        uma tecnologia do Instituto Ponte para desenvolver estudantes
+        talentosos, moradores de cidades do interior e que sonham completar os
+        estudos na capital. No Ensino Fundamental, os alunos permanecem em suas
+        cidades, junto a suas famílias, realizam curso de inglês em escolas
+        parceiras, acompanhamento pedagógico e psicológico, e aulas virtuais ou
+        presenciais. No Ensino Médio, podem se mudar para a Grande Vitória
+        tornando-se bolsista em alguma escola particular parceira do Instituto
+        Ponte.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
