a026c02aec092666b26971be7e4eb5f6cb3be6fb
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-07-05 20:00:36 -0300

    Criado componente de trabalhos recentes

---
+++ src/components/RecentWorks.vue
@@ -0,0 +1,100 @@
+.<template>
+  <div class="mt-5">
+    <h1 class=" mt-5 text-center">
+      Veja os trabalhos recentes adicionados
+    </h1>
+    <div class="">
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-card color="transparent" flat class="mx-auto">
+            <v-img
+              dark
+              width="490"
+              gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+              class="white--text align-end bottom-gradient"
+              height="400"
+              src="https://www12.senado.leg.br/noticias/materias/2019/05/07/reforma-da-previdencia-cria-contribuicao-minima-para-trabalhadores-do-campo/trabalhadora_rural_3.jpg/@@images/image/imagem_materia"
+            >
+              <div class="pa-5">
+                <v-chip outlined color="white" class="ma-2"
+                  >#Agricultura</v-chip
+                >
+                <v-chip outlined color="white" class="ma-2">#Famílias</v-chip>
+                <h2 class="mx-auto ml-2">
+                  Trabalhe ajudando a agricultura
+                </h2>
+              </div>
+            </v-img>
+          </v-card>
+        </v-col>
+        <v-col cols="auto">
+          <v-card color="transparent" flat class="mx-auto">
+            <v-img
+              dark
+              width="490"
+              gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+              class="white--text align-end bottom-gradient"
+              height="400"
+              src="https://f.i.uol.com.br/fotografia/2020/03/21/15847630095e7590818e3b5_1584763009_3x2_xl.jpg"
+            >
+              <div class="pa-5">
+                <v-chip outlined color="white" class="ma-2">#Pessoas</v-chip>
+                <v-chip outlined color="white" class="ma-2">#Vidas</v-chip>
+                <h2 class="mx-auto ml-2">
+                  Trabalhe ajudando moradores de rua
+                </h2>
+              </div>
+            </v-img>
+          </v-card>
+        </v-col>
+        <v-col cols="auto">
+          <v-card color="transparent" flat class="mx-auto">
+            <v-img
+              dark
+              width="490"
+              gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+              class="white--text align-end bottom-gradient"
+              height="400"
+              src="https://f.i.uol.com.br/fotografia/2018/07/26/15326347775b5a26993378c_1532634777_3x2_lg.jpg"
+            >
+              <div class="pa-5">
+                <v-chip outlined color="white" class="ma-2">#Voluntário</v-chip>
+                <v-chip outlined color="white" class="ma-2">#Animais</v-chip>
+                <h2 class="mx-auto ml-2">
+                  Trabalhe salvando animais de rua
+                </h2>
+              </div>
+            </v-img>
+          </v-card>
+        </v-col>
+        <v-col cols="auto">
+          <v-card color="transparent" flat class="mx-auto">
+            <v-img
+              dark
+              width="490"
+              gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+              class="white--text align-end bottom-gradient"
+              height="400"
+              src="https://f.i.uol.com.br/folha/cotidiano/images/16251160.jpeg"
+            >
+              <div class="pa-5">
+                <v-chip outlined color="white" class="ma-2">#Pandas</v-chip>
+                <v-chip outlined color="white" class="ma-2">#Animais</v-chip>
+
+                <h2 class="mx-auto ml-2">
+                  Trabalhe salvando pandas na china
+                </h2>
+              </div>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
